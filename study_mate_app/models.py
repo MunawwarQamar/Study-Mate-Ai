b5490da9edb360e3ff574c420c21d29fedbeabd7
baselalam10-art <baselalam30@gmail.com>
2026-04-02 17:35:26 +0200

    Add subjects, study plan and tasks pages with AI integration and AJAX

--- study_mate_app/models.py
+++ study_mate_app/models.py
@@ -63,6 +63,12 @@ class StudyPlanItem(models.Model):
 
     study_date = models.DateField()
     planned_hours = models.DecimalField(max_digits=4, decimal_places=2)
+    STATUS_CHOICES = [
+        {'pending' , 'Pending'},
+        ('completed' , 'Completed')
+    ]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- study_mate_app/urls.py
+++ study_mate_app/urls.py
@@ -2,5 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Subjects
     path('', views.home),
+    path('subjects/' , views.all_subjects, name='all_subjects'),
+    path('subjects/add/' , views.add_subject, name='add_subject'),
+    path('subjects/edit/<int:id>/' , views.edit_subject, name='edit_subject'),
+    path('subjects/delete/<int:id>/' , views.delete_subject, name='delete_subject'),
+    # Study Plan
+    path('study-plan/' , views.study_plan, name='study_plan'),
+    path('study-plan/generate' , views.generate_plan, name='generate_plan'),
+    # Tasks
+    path('tasks/' , views.all_tasks, name='all_tasks'),
+    path('tasks/toggle/<int:id>/' , views.toggle_task, name='toggle_task'),
+
+
+
 ]
