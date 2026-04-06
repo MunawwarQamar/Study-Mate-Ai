ba66ea15a5cfa064f386e845c4bf535ccbe88b06
Majd Kawa <majd.mkawa@gmail.com>
2026-04-06 11:35:37 +0300

    Upgrading the login/register page by adding a tab and JS to switch

--- study_mate_app/urls.py
+++ study_mate_app/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test_page),
-]
-    path('', views.home),
     path('auth/', views.auth_page, name='auth'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
